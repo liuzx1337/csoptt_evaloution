f4c2b77c8a0c9f1e58a12a0dd310064241fae9f3
liuzixi <liu.zixi@ebiz-fooge.com>
2018-12-22 18:03:57 +0800

    体系建立

---
+++ csoptt_evaloution-restful/src/main/java/com/csoptt/custom/entity/BaseEvaluation.java
@@ -0,0 +1,30 @@
+package com.csoptt.custom.entity;
+
+/**
+ * 评测基类
+ *
+ * @author liuzixi
+ * @date 2018-12-22
+ */
+public class BaseEvaluation {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 简称
+     */
+    private String shortName;
+
+    /**
+     * 作者
+     */
+    private String author;
+}
